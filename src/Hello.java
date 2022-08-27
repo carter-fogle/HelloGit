a00eba3e666cbbf0dbb423c3516ed8ecc0c7f5b7
csfog <csfog@DESKTOP-3E51IMB>
2022-08-27 13:58:11 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * 2022/08/25
+ * 
+ * @author csfog or Carter Fogle
+ * 
+ * Program is being used to help my understanding of git repos
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Carter Fogle");
+		System.out.println("Computer Engineering");
+		
+
+	}
+
+}
